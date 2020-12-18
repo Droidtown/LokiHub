dcbfc5a8cda18108bd9b19e0f09380a303db0fda
howardsukuan <56188677+howardsukuan@users.noreply.github.com>
2020-12-18 16:57:01 +0800

    讓他可以真的換錢

--- FinBot/FinExchange.py
+++ FinBot/FinExchange.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
 """
     Loki 2.0 Template For Python3
+
+    [URL] https://api.droidtown.co/Loki/BulkAPI/
 
     Request:
         {
             "username": "your_username",
-            "input_str": "your_input",
+            "input_list": ["your_input_1", "your_input_2"],
             "loki_key": "your_loki_key",
             "filter_list": ["intent_filter_list"] # optional
         }
@@ -16,60 +17,67 @@
         {
             "status": True,
             "msg": "Success!",
-            "version": "v224",
+            "version": "v223",
             "word_count_balance": 2000,
-            "results": [
+            "result_list": [
                 {
-                    "intent": "intentName",
-                    "pattern": "matchPattern",
-                    "utterance": "matchUtterance",
-                    "argument": ["arg1", "arg2", ... "argN"]
+                    "status": True,
+                    "msg": "Success!",
+                    "results": [
+                        {
+                            "intent": "intentName",
+                            "pattern": "matchPattern",
+                            "utterance": "matchUtterance",
+                            "argument": ["arg1", "arg2", ... "argN"]
+                        },
+                        ...
+                    ]
                 },
-                ...
+                {
+                    "status": False,
+                    "msg": "No Match Intent!"
+                }
             ]
         }
 """
 
-import json
-import os
 import requests
 try:
     from intent import Loki_Exchange
 except:
     from .intent import Loki_Exchange
 
-try:
-    infoPath = "{}/account.info".format(os.path.dirname(os.path.abspath(__file__))).replace("/FinBot", "")
-    infoDICT = json.load(open(infoPath, "r"))
-    USERNAME = infoDICT["username"]
-    API_KEY = infoDICT["api_key"]
-    LOKI_KEY = infoDICT["FinBot_key"]
-except:
-    # HINT: 在這裡填入您在 https://api.droidtown.co 的帳號、Articut 的 API_Key 以及 Loki 專案的 Loki_Key
-    USERNAME = ""
-    API_KEY = ""
-    LOKI_KEY = ""
+from ArticutAPI import ArticutAPI
+articut = ArticutAPI.Articut()
 
+LOKI_URL = "https://api.droidtown.co/Loki/BulkAPI/"
+USERNAME = ""
+LOKI_KEY = ""
+# 意圖過濾器說明
+# INTENT_FILTER = []        => 比對全部的意圖 (預設)
+# INTENT_FILTER = [intentN] => 僅比對 INTENT_FILTER 內的意圖
+INTENT_FILTER = []
 
 class LokiResult():
     status = False
     message = ""
     version = ""
     balance = -1
-    lokiResultLIST = None
+    lokiResultLIST = []
 
-    def __init__(self, input_str):
+    def __init__(self, inputLIST):
         self.status = False
         self.message = ""
         self.version = ""
         self.balance = -1
-        self.lokiResultLIST = None
+        self.lokiResultLIST = []
 
         try:
-            result = requests.post("https://api.droidtown.co/Loki/API/", json={
+            result = requests.post(LOKI_URL, json={
                 "username": USERNAME,
-                "input_str": input_str,
-                "loki_key": LOKI_KEY
+                "input_list": inputLIST,
+                "loki_key": LOKI_KEY,
+                "filter_list": INTENT_FILTER
             })
 
             if result.status_code == requests.codes.ok:
@@ -79,9 +87,9 @@ class LokiResult():
                 if result["status"]:
                     self.version = result["version"]
                     self.balance = result["word_count_balance"]
-                    self.lokiResultLIST = result["results"]
+                    self.lokiResultLIST = result["result_list"]
             else:
-                self.message = "Connect Error."
+                self.message = "Connect failed."
         except Exception as e:
             self.message = str(e)
 
@@ -97,114 +105,127 @@ class LokiResult():
     def getBalance(self):
         return self.balance
 
-    def getLen(self):
-        rst = 0
-        if self.lokiResultLIST is not None:
-            rst = len(self.lokiResultLIST)
-
+    def getLokiStatus(self, index):
+        rst = False
+        if index < len(self.lokiResultLIST):
+            rst = self.lokiResultLIST[index]["status"]
         return rst
 
-    def getLokiResult(self, index):
-        lokiResultDICT = None
-        if self.lokiResultLIST is not None:
-            if index < self.getLen():
-                lokiResultDICT = self.lokiResultLIST[index]
+    def getLokiMessage(self, index):
+        rst = ""
+        if index < len(self.lokiResultLIST):
+            rst = self.lokiResultLIST[index]["msg"]
+        return rst
 
+    def getLokiLen(self, index):
+        rst = 0
+        if index < len(self.lokiResultLIST):
+            if self.lokiResultLIST[index]["status"]:
+                rst = len(self.lokiResultLIST[index]["results"])
+        return rst
+
+    def getLokiResult(self, index, resultIndex):
+        lokiResultDICT = None
+        if resultIndex < self.getLokiLen(index):
+            lokiResultDICT = self.lokiResultLIST[index]["results"][resultIndex]
         return lokiResultDICT
 
-    def getIntent(self, index):
+    def getIntent(self, index, resultIndex):
         rst = ""
-        lokiResultDICT = self.getLokiResult(index)
-        if lokiResultDICT is not None:
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
             rst = lokiResultDICT["intent"]
-
         return rst
 
-    def getPattern(self, index):
+    def getPattern(self, index, resultIndex):
         rst = ""
-        lokiResultDICT = self.getLokiResult(index)
-        if lokiResultDICT is not None:
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
             rst = lokiResultDICT["pattern"]
-
         return rst
 
-    def getUtterance(self, index):
+    def getUtterance(self, index, resultIndex):
         rst = ""
-        lokiResultDICT = self.getLokiResult(index)
-        if lokiResultDICT is not None:
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
             rst = lokiResultDICT["utterance"]
-
         return rst
 
-    def getArgs(self, index):
+    def getArgs(self, index, resultIndex):
         rst = []
-        lokiResultDICT = self.getLokiResult(index)
-        if lokiResultDICT is not None:
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
             rst = lokiResultDICT["argument"]
-
         return rst
 
-#建立貨幣名稱代碼轉換字典
-currencyCodeDICT = {"美金":"USD", "美元":"USD", "日幣":"JPY", "日圓":"JPY", "日元":"JPY",
-                    "台幣":"TWD", "臺幣":"TWD", "新台幣":"TWD", "新臺幣":"TWD", "歐元":"EUR"}
-
-#建立貨幣匯率字典
-currencyExRateDICT = {}
-
-def getCurrencyExRate():
-    '''取得最新匯率，並儲存到 currencyExRateDICT'''
-    global currencyExRateDICT
-    currencyExRateDICT = requests.get("https://tw.rter.info/capi.php").json()
-
-def getSrc2TgtCurrencyExRate(sourceCurrencySTR, targetCurrencySTR):
-    '''依 currencyExRateDICT 的內容，將 sourceCurrencySTR 和 targetCurrencySTR 兩種貨幣之間的匯率取出並回傳'''
-    if sourceCurrencySTR == None:
-        sourceCurrencyCode = "TWD"
-    else:
-        sourceCurrencyCode = currencyCodeDICT[sourceCurrencySTR]
-
-    if targetCurrencySTR == None:
-        targetCurrencyCode = "TWD"
-    else:
-        targetCurrencyCode = currencyCodeDICT[targetCurrencySTR]
-    #print(sourceCurrencyCode, targetCurrencyCode)
-    exRate = (1 / currencyExRateDICT["USD{}".format(sourceCurrencyCode)]["Exrate"]) * (currencyExRateDICT["USD{}".format(targetCurrencyCode)]["Exrate"])
-    return exRate
-
-def amountSTRconvert(amountSTR):
-    '''把 amountSTR 的金額數字字串轉為數值類型並回傳'''
-    if amountSTR:
-        response = requests.post("https://api.droidtown.co/Articut/API/",
-                                  json={"username": USERNAME,
-                                        "api_key": API_KEY,
-                                        "input_str": amountSTR,
-                                        "version": "latest",
-                                        "level": "lv3",
-                                        }).json()
-        return response["number"][amountSTR]
-    else:
-        return 1
-
-def runLoki(input_str):
+def runLoki(inputLIST):
     resultDICT = {}
-    lokiRst = LokiResult(input_str)
+    lokiRst = LokiResult(inputLIST)
     if lokiRst.getStatus():
-        for i in range(0, lokiRst.getLen()):
-            # Exchange
-            if lokiRst.getIntent(i) == "Exchange":
-                resultDICT = Loki_Exchange.getResult(lokiRst.getPattern(i), lokiRst.getUtterance(i), lokiRst.getArgs(i), resultDICT)
-        getCurrencyExRate()
-        # 兌換數字 * 貨幣轉換匯率
-        resultDICT["answer"] = amountSTRconvert(resultDICT["amount"]) * getSrc2TgtCurrencyExRate(resultDICT["source"], resultDICT["target"])
+        for index, key in enumerate(inputLIST):
+            for resultIndex in range(0, lokiRst.getLokiLen(index)):
+                # Exchange
+                if lokiRst.getIntent(index, resultIndex) == "Exchange":
+                    resultDICT = Loki_Exchange.getResult(key, lokiRst.getUtterance(index, resultIndex), lokiRst.getArgs(index, resultIndex), resultDICT)
+
     else:
         resultDICT = {"msg": lokiRst.getMessage()}
-
     return resultDICT
 
-if __name__ == "__main__":
-    input_str = "100美金換台幣"
-    resultDICT = runLoki(input_str)
-    if "answer" in resultDICT:
-        print("=> {} 元".format(resultDICT["answer"]))
+moneyDICT = {'台幣': 'TWD','新台幣':'TWD','美元':'USD','鎂':'USD', '美金': 'USD', '歐元': 'EUR', '日圓': 'JPY', '英鎊': 'GBP', '瑞士法郎': 'CHF', '加幣': 'CAD', '澳幣': 'AUD', '巴西黑奧': 'BRL', '人民幣': 'CNY', '捷克克朗': 'CZK', '丹麥克羅納': 'DKK', '港幣': 'HKD', '匈牙利': 'HUF', '印尼盧比': 'IDR', '印度盧比': 'INR', '韓圜': 'KRW', '墨西哥披索': 'MXN', '挪威克?': 'NOK', '紐幣': 'NZD', '菲律賓披索': 'PHP', '波蘭茲羅提': 'PLN', '俄國盧布': 'RUB', '瑞典克郎': 'SEK', '新加坡幣': 'SGD', '泰銖': 'THB', '土耳其里拉': 'TRL', '南非幣': 'ZAR', '馬來西亞': 'MYR', '斯洛伐克克朗': 'SKK', '沙烏地阿拉伯利雅': 'SAR', '哥倫比亞披索': 'COP', '辛巴威': 'ZWD', '摩洛哥迪拉姆': 'MAD', '埃及鎊': 'EGP', '以色列謝克': 'ILS', '智利披索': 'CLP', '阿根廷披索': 'ARS', '玻利維亞幣': 'BOB', '厄爪多爾蘇克雷': 'ECS', '巴拿馬巴布亞': 'PAB', '委內瑞拉銀幣': 'VEB', '巴基斯坦盧比': 'PKR', '斯里蘭卡盧比': 'LKR', '哥斯大黎加': 'CRC', '新土耳其里拉': 'TRY', '肯亞先令': 'KES', '模里西斯盧比': 'MUR', '越南幣': 'VND', '冰島幣': 'ISK', '阿拉伯聯合大公國迪拉姆': 'AED', '秘魯索爾': 'PEN', '黃金': 'XAU', '境外人民幣': 'CNH', '科威特幣': 'KWD', '澳門幣': 'MOP'}
+def moneyName(inputSTR):
+    return moneyDICT[inputSTR]
+
+    
+#dict from
+#import csv
+#curDICT={}
+#with open('currency_CE.csv', 'r') as file:
+#    reader = csv.reader(file)
+#    for row in reader:
+#         curDICT[row[1]]=row[0]
+
+
+def amountSTRConvert(inputSTR):
+    resultDICT = articut.parse(inputSTR, level="lv3") 
+    return resultDICT["number"]
+
+def ListToString(MyInput):
+    if isinstance(MyInput,str):
+        return(MyInput)  
     else:
-        print(resultDICT["msg"])
+        return(MyInput[0])
+    
+    
+if __name__ == "__main__":
+    inputLIST = ["一百歐元可以換多少台幣?"]
+    resultDICT = runLoki(inputLIST)
+    #try "今天美金換台幣是多少" "我想要100鎂" "一百美元" "一百元美元"
+    
+     
+    #converting
+    source_1 = ListToString(resultDICT["src"])
+    target_1 = ListToString(resultDICT["tgt"])
+    amount_1 = ListToString(resultDICT["amt"])
+    #dealing with 一百美元
+    
+    
+    #get today's rate
+    response = requests.get("https://tw.rter.info/capi.php")
+    rateDICT = response.json()
+
+    #change money name from Chinese to English, and amount to numbers
+    Source= "USD"+moneyName(source_1)
+    Target = "USD"+moneyName(target_1)
+    Amount = amountSTRConvert(amount_1 )[amount_1]
+    
+    
+    #search in dictionary
+    Source_rate = rateDICT[Source]["Exrate"]
+    Target_rate = rateDICT[Target]["Exrate"]
+    #Exchange!
+    Result = Amount*(Target_rate/Source_rate)
+    
+    print("您好~~\n{0}{1}可換得%2.2f{2}\n歡迎再次使用!" .format(Amount,source_1,target_1)%(Result))
+
+#use request to download the data from: https://tw.rter.info/capi.php
--- FinBot/intent/Loki_Exchange.py
+++ FinBot/intent/Loki_Exchange.py
@@ -3,124 +3,244 @@
 
 """
     Loki module for Exchange
-
+    
     Input:
-        pattern       str,
+        inputSTR      str,
+        utterance     str,
         args          str[],
         resultDICT    dict
-
+    
     Output:
         resultDICT    dict
 """
 
 DEBUG_Exchange = True
+userDefinedDICT = {}
+import re
 
-#建立貨幣名稱代碼轉換字典
-currencyCodeDICT = {"美金":"USD", "美元":"USD", "日幣":"JPY", "日圓":"JPY", "日元":"JPY",
-                    "台幣":"TWD", "臺幣":"TWD", "新台幣":"TWD", "新臺幣":"TWD", "歐元":"EUR"}
+#creat a dictionary first
+moneyDICT = {'台幣': 'TWD','新台幣':'TWD','美元':'USD','鎂':'USD', '美金': 'USD', '歐元': 'EUR', '日圓': 'JPY', '英鎊': 'GBP', '瑞士法郎': 'CHF', '加幣': 'CAD', '澳幣': 'AUD', '巴西黑奧': 'BRL', '人民幣': 'CNY', '捷克克朗': 'CZK', '丹麥克羅納': 'DKK', '港幣': 'HKD', '匈牙利': 'HUF', '印尼盧比': 'IDR', '印度盧比': 'INR', '韓圜': 'KRW', '墨西哥披索': 'MXN', '挪威克?': 'NOK', '紐幣': 'NZD', '菲律賓披索': 'PHP', '波蘭茲羅提': 'PLN', '俄國盧布': 'RUB', '瑞典克郎': 'SEK', '新加坡幣': 'SGD', '泰銖': 'THB', '土耳其里拉': 'TRL', '南非幣': 'ZAR', '馬來西亞': 'MYR', '斯洛伐克克朗': 'SKK', '沙烏地阿拉伯利雅': 'SAR', '哥倫比亞披索': 'COP', '辛巴威': 'ZWD', '摩洛哥迪拉姆': 'MAD', '埃及鎊': 'EGP', '以色列謝克': 'ILS', '智利披索': 'CLP', '阿根廷披索': 'ARS', '玻利維亞幣': 'BOB', '厄爪多爾蘇克雷': 'ECS', '巴拿馬巴布亞': 'PAB', '委內瑞拉銀幣': 'VEB', '巴基斯坦盧比': 'PKR', '斯里蘭卡盧比': 'LKR', '哥斯大黎加': 'CRC', '新土耳其里拉': 'TRY', '肯亞先令': 'KES', '模里西斯盧比': 'MUR', '越南幣': 'VND', '冰島幣': 'ISK', '阿拉伯聯合大公國迪拉姆': 'AED', '秘魯索爾': 'PEN', '黃金': 'XAU', '境外人民幣': 'CNH', '科威特幣': 'KWD', '澳門幣': 'MOP'}
 
+
+currencyLIST = []
+for x in moneyDICT:
+    currencyLIST.append(x)
+    currencyLIST.extend(moneyDICT[x])
+        
+def GetCurrency(inputSTR):
+    for currency in currencyLIST:
+        if currency in inputSTR:
+            return currency        
+    
 # 將符合句型的參數列表印出。這是 debug 或是開發用的。
-def debugInfo(pattern, utterance, args):
+def debugInfo(inputSTR, utterance):
     if DEBUG_Exchange:
-        print("[Exchange]\n{} ===> {}\n{}\n".format(utterance, args, pattern))
+            print("[Exchange] {} ===> {}".format(inputSTR, utterance))
 
-def getResult(pattern, utterance, args, resultDICT):
-    debugInfo(pattern, utterance, args)
-
-    if utterance == "[我]想要[美金][100元]":
-        resultDICT["source"] = args[1]
-        resultDICT["target"] = None
-        resultDICT["amount"] = args[2]
-
-    if utterance == "[我]想要[100元][美金]":
-        resultDICT["source"] = args[2]
-        resultDICT["target"] = None
-        resultDICT["amount"] = args[1]
-
-    if utterance == "[我]想買[美金][100元]":
-        resultDICT["source"] = args[1]
-        resultDICT["target"] = None
-        resultDICT["amount"] = args[2]
-
-    if utterance == "[我]想買[100元][美金]":
-        resultDICT["source"] = args[2]
-        resultDICT["target"] = None
-        resultDICT["amount"] = args[1]
-
-    if utterance == "[美金][100]要多少[台幣]":
-        resultDICT["source"] = args[0]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[1]
-
-    if utterance == "[美金][100]要[台幣]多少":
-        resultDICT["source"] = args[0]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[1]
-
-    if utterance == "[美金][100元]要多少[台幣]":
-        resultDICT["source"] = args[0]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[1]
-
-    if utterance == "[美金][100元]要[台幣]多少":
-        resultDICT["source"] = args[0]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[1]
-
-    if utterance == "[美金][100元]可以兌換多少[台幣]":
-        resultDICT["source"] = args[0]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[1]
-
-    if utterance == "[美金][100元]可以兌換[台幣]多少":
-        resultDICT["source"] = args[0]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[1]
-
-    if utterance == "[100美金]要多少[台幣]":
-        resultDICT["source"] = [x for x in currencyCodeDICT if x in args[0]][0]
-        resultDICT["target"] = args[1]
-        resultDICT["amount"] = args[0]
-
-    if utterance == "[100美金]要[台幣]多少":
-        resultDICT["source"] = [x for x in currencyCodeDICT if x in args[0]][0]
-        resultDICT["target"] = args[1]
-        resultDICT["amount"] = args[0]
-
-    if utterance == "[100美金]能換多少[台幣]":
-        resultDICT["source"] = [x for x in currencyCodeDICT if x in args[0]][0]
-        resultDICT["target"] = args[1]
-        resultDICT["amount"] = args[0]
-
-    if utterance == "[100元][美金]要多少[台幣]":
-        debugInfo("[100元][美金]要多少[台幣]", args)
-        resultDICT["source"] = args[1]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[0]
-
-    if utterance == "[100元][美金]要[台幣]多少":
-        resultDICT["source"] = args[1]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[0]
+def getResult(inputSTR, utterance, args, resultDICT):
+    debugInfo(inputSTR, utterance)
+    if utterance == "[100元][美金]可以兌換[台幣]多少":
+        # write your code here
+        resultDICT["src"]=args[1]
+        resultDICT["tgt"]=args[2]
+        resultDICT["amt"]=args[0]
+        pass
 
     if utterance == "[100元][美金]可以兌換多少[台幣]":
-        resultDICT["source"] = args[1]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[0]
+        # write your code here
+        resultDICT["src"]=args[1]
+        resultDICT["tgt"]=args[2]
+        resultDICT["amt"]=args[0]        
+        pass
 
-    if utterance == "[100元][美金]可以兌換[台幣]多少":
-        resultDICT["source"] = args[1]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = args[0]
+    if utterance == "[100元][美金]要[台幣]多少":
+        # write your code here
+        resultDICT["src"]=args[1]
+        resultDICT["tgt"]=args[2]
+        resultDICT["amt"]=args[0]        
+        pass
+
+    if utterance == "[100元][美金]要多少[台幣]":
+        # write your code here
+        resultDICT["src"]=args[1]
+        resultDICT["tgt"]=args[2]
+        resultDICT["amt"]=args[0]        
+        pass
 
     if utterance == "[100台幣]換[美金]":
-        resultDICT["source"] = [x for x in currencyCodeDICT if x in args[0]][0]
-        resultDICT["target"] = args[1]
-        resultDICT["amount"] = args[0]
+        # write your code here
+        resultDICT["src"]=GetCurrency(args[0])
+        resultDICT["tgt"]=args[1]
+        resultDICT["amt"]=args[0]    
+        pass
+
+    if utterance == "[100美金]能換多少[台幣]":
+        # write your code here
+        resultDICT["src"]=GetCurrency(args[0])
+        resultDICT["tgt"]=args[1]
+        resultDICT["amt"]=args[0]      
+        pass
+#如果是文字的一百美金感覺會比較複雜
+    if utterance == "[100美金]要[台幣]多少":
+        # write your code here
+        resultDICT["src"]=GetCurrency(args[0])
+        resultDICT["tgt"]=args[1]
+        resultDICT["amt"]=args[0]       
+        pass
+
+    if utterance == "[100美金]要多少[台幣]":
+        # write your code here
+        resultDICT["src"]=GetCurrency(args[0])
+        resultDICT["tgt"]=args[1]
+        resultDICT["amt"]=args[0]       
+        pass
 
     if utterance == "[今天][美金]兌換[台幣]是多少":
-        resultDICT["source"] = args[1]
-        resultDICT["target"] = args[2]
-        resultDICT["amount"] = None
+        # write your code here
+        resultDICT["src"]= args[1]
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= "1"        
+        pass
 
+    if utterance == "[我]想要換[100元][美金]":
+        # write your code here
+        #當有缺漏的時候你可以把那個設成default接著在function中設定為台幣
+        #你也可以設成 if arg in ["我"]: 來限制你想要的對象
+        resultDICT["src"]= "台幣"
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= args[1]    
+        pass
+
+    if utterance == "[我]想要[美金][100元]":
+        # write your code here
+        resultDICT["src"]= "台幣"
+        resultDICT["tgt"]= args[1]
+        resultDICT["amt"]=  args[2]           
+        pass
+
+    if utterance == "[我]想買[100元][美金]":
+        # write your code here
+        resultDICT["src"]= "台幣"
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]=  args[1]           
+        pass
+
+    if utterance == "[我]想買[美金][100元]":
+        # write your code here
+        resultDICT["src"]= "台幣"
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]=  args[1]          
+        pass
+
+    if utterance == "[美金][100]要[台幣]多少":
+        # write your code here
+        resultDICT["src"]= args[0]
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= args[1]
+        pass
+
+    if utterance == "[美金][100]要多少[台幣]":
+        # write your code here
+        resultDICT["src"]= args[0]
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= args[1]        
+        pass
+
+    if utterance == "[美金][100元]可以兌換[台幣]多少":
+        # write your code here
+        resultDICT["src"]= args[0]
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= args[1]        
+        pass
+
+    if utterance == "[美金][100元]可以兌換多少[台幣]":
+        # write your code here
+        resultDICT["src"]= args[0]
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= args[1]        
+        pass
+
+    if utterance == "[美金][100元]要[台幣]多少":
+        # write your code here
+        resultDICT["src"]= args[0]
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= args[1]        
+        pass
+
+    if utterance == "[美金][100元]要多少[台幣]":
+        # write your code here
+        resultDICT["src"]= args[0]
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= args[1]        
+        pass
+    
+    if utterance == "[100元][台幣]換[美金]":
+        resultDICT["src"]= args[1]
+        resultDICT["tgt"]= args[2]
+        resultDICT["amt"]= args[0]
+        pass
+    
+    if utterance == "[我]想換[200美金]":
+        # args [我, 200美金]  
+        resultDICT["src"]= re.findall("[^\\d]+",args[1])
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= re.findall("\\d+",args[1])
+        pass
+    
+    if utterance == "[100美金]怎麼換":
+        # args [100美金]
+        resultDICT["src"]= re.findall("[^\\d]+",args[0])
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= re.findall("\\d+",args[0])
+        pass
+        
+    if utterance == "[我]想兌換[200元][美金]":
+        # args [我, 200元, 美金]
+        resultDICT["src"]= args[2]
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= args[1]
+        pass
+    
+    if utterance == "[我]要兌換[200元][美金]":
+        # args [我, 200元, 美金] 
+        resultDICT["src"]= args[2]
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= args[1]
+        pass
+    
+    if utterance == "[我]可以換一下[200元][美金]":
+        # args [我, 200元, 美金] 
+        resultDICT["src"]= args[2]
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= args[1]
+        
+    if utterance == "[我]可以換[個][200元][美金]":
+        # args [我, 個, 200元, 美金]  
+        resultDICT["src"]= args[2]
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= args[1]
+        pass
+        
+    if utterance == "[我]想要換一下[100元][美金]":
+        # args [我, 100元, 美金]
+        resultDICT["src"]= args[2] 
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= args[1]           
+        pass
+    
+    if utterance == "[我]想要換[個][100元][美金]":
+        # args [我, 個, 100元, 美金]
+        resultDICT["src"]= args[3] 
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= args[2]           
+        pass
+        
+    if utterance == "[我]想要[100][鎂]":
+         # args [我, 100, 鎂]
+        resultDICT["src"]= args[2] 
+        resultDICT["tgt"]= "台幣"
+        resultDICT["amt"]= args[1]           
+        pass
+        
     return resultDICT
+
