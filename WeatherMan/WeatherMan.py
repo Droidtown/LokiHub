3b4e2387b867a5ced441ed01cbea8d4a89ac78c8
Eclair <eclair.c@droidtown.co>
2021-06-09 11:08:36 +0800

    WeatherMan updates to Loki 2.0

--- WeatherMan/WeatherMan.py
+++ WeatherMan/WeatherMan.py
@@ -4,10 +4,12 @@
 """
     Loki 2.0 Template For Python3
 
+    [URL] https://api.droidtown.co/Loki/BulkAPI/
+
     Request:
         {
             "username": "your_username",
-            "input_str": "your_input",
+            "input_list": ["your_input_1", "your_input_2"],
             "loki_key": "your_loki_key",
             "filter_list": ["intent_filter_list"] # optional
         }
@@ -16,71 +18,95 @@
         {
             "status": True,
             "msg": "Success!",
-            "version": "v224",
+            "version": "v223",
             "word_count_balance": 2000,
-            "results": [
+            "result_list": [
                 {
-                    "intent": "intentName",
-                    "pattern": "matchPattern",
-                    "utterance": "matchUtterance",
-                    "argument": ["arg1", "arg2", ... "argN"]
+                    "status": True,
+                    "msg": "Success!",
+                    "results": [
+                        {
+                            "intent": "intentName",
+                            "pattern": "matchPattern",
+                            "utterance": "matchUtterance",
+                            "argument": ["arg1", "arg2", ... "argN"]
+                        },
+                        ...
+                    ]
                 },
-                ...
+                {
+                    "status": False,
+                    "msg": "No Match Intent!"
+                }
             ]
         }
 """
 
+from pprint import pprint
+from requests import post
+from requests import codes
 import json
+import math
 import os
-import requests
 try:
     from intent import Loki_Weather
 except:
     from .intent import Loki_Weather
 
+BASEPATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+LOKI_URL = "https://api.droidtown.co/Loki/BulkAPI/"
+
 try:
-    infoPath = "{}/account.info".format(os.path.dirname(os.path.abspath(__file__))).replace("/WeatherMan", "")
+    infoPath = "{}/account.info".format(BASEPATH.replace("/WeatherMan", ""))
     infoDICT = json.load(open(infoPath, "r"))
     USERNAME = infoDICT["username"]
-    API_KEY = infoDICT["api_key"]
     LOKI_KEY = infoDICT["WeatherMan_loki_key"]
 except:
     # HINT: 在這裡填入您在 https://api.droidtown.co 的帳號、Articut 的 API_Key 以及 Loki 專案的 Loki_Key
     USERNAME = ""
-    API_KEY = ""
     LOKI_KEY = ""
+
+# 意圖過濾器說明
+# INTENT_FILTER = []        => 比對全部的意圖 (預設)
+# INTENT_FILTER = [intentN] => 僅比對 INTENT_FILTER 內的意圖
+INTENT_FILTER = []
 
 class LokiResult():
     status = False
     message = ""
     version = ""
     balance = -1
-    lokiResultLIST = None
+    lokiResultLIST = []
 
-    def __init__(self, input_str):
+    def __init__(self, inputLIST, filterLIST):
         self.status = False
         self.message = ""
         self.version = ""
         self.balance = -1
-        self.lokiResultLIST = None
+        self.lokiResultLIST = []
+        # filterLIST 空的就採用預設的 INTENT_FILTER
+        if filterLIST == []:
+            filterLIST = INTENT_FILTER
 
         try:
-            result = requests.post("https://api.droidtown.co/Loki/API/", json={
+            result = post(LOKI_URL, json={
                 "username": USERNAME,
-                "input_str": input_str,
-                "loki_key": LOKI_KEY
+                "input_list": inputLIST,
+                "loki_key": LOKI_KEY,
+                "filter_list": filterLIST
             })
 
-            if result.status_code == requests.codes.ok:
+            if result.status_code == codes.ok:
                 result = result.json()
                 self.status = result["status"]
                 self.message = result["msg"]
                 if result["status"]:
                     self.version = result["version"]
                     self.balance = result["word_count_balance"]
-                    self.lokiResultLIST = result["results"]
+                    self.lokiResultLIST = result["result_list"]
             else:
-                self.message = "Connect Error."
+                self.message = "Connect failed."
         except Exception as e:
             self.message = str(e)
 
@@ -96,75 +122,98 @@ class LokiResult():
     def getBalance(self):
         return self.balance
 
-    def getLen(self):
-        rst = 0
-        if self.lokiResultLIST is not None:
-            rst = len(self.lokiResultLIST)
-
+    def getLokiStatus(self, index):
+        rst = False
+        if index < len(self.lokiResultLIST):
+            rst = self.lokiResultLIST[index]["status"]
         return rst
 
-    def getLokiResult(self, index):
-        lokiResultDICT = None
-        if self.lokiResultLIST is not None:
-            if index < self.getLen():
-                lokiResultDICT = self.lokiResultLIST[index]
+    def getLokiMessage(self, index):
+        rst = ""
+        if index < len(self.lokiResultLIST):
+            rst = self.lokiResultLIST[index]["msg"]
+        return rst
 
+    def getLokiLen(self, index):
+        rst = 0
+        if index < len(self.lokiResultLIST):
+            if self.lokiResultLIST[index]["status"]:
+                rst = len(self.lokiResultLIST[index]["results"])
+        return rst
+
+    def getLokiResult(self, index, resultIndex):
+        lokiResultDICT = None
+        if resultIndex < self.getLokiLen(index):
+            lokiResultDICT = self.lokiResultLIST[index]["results"][resultIndex]
         return lokiResultDICT
 
-    def getIntent(self, index):
+    def getIntent(self, index, resultIndex):
         rst = ""
-        lokiResultDICT = self.getLokiResult(index)
-        if lokiResultDICT is not None:
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
             rst = lokiResultDICT["intent"]
-
         return rst
 
-    def getPattern(self, index):
+    def getPattern(self, index, resultIndex):
         rst = ""
-        lokiResultDICT = self.getLokiResult(index)
-        if lokiResultDICT is not None:
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
             rst = lokiResultDICT["pattern"]
-
         return rst
 
-    def getUtterance(self, index):
+    def getUtterance(self, index, resultIndex):
         rst = ""
-        lokiResultDICT = self.getLokiResult(index)
-        if lokiResultDICT is not None:
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
             rst = lokiResultDICT["utterance"]
-
         return rst
 
-    def getArgs(self, index):
+    def getArgs(self, index, resultIndex):
         rst = []
-        lokiResultDICT = self.getLokiResult(index)
-        if lokiResultDICT is not None:
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
             rst = lokiResultDICT["argument"]
-
         return rst
 
-def runLoki(input_str):
+def runLoki(inputLIST, filterLIST=[]):
     resultDICT = {}
-
     #視需要做些正規化的操作 (例如「台/臺」或是 "11:30" 變為「十一點三十」)
-    lokiRst = LokiResult(input_str.replace("台", "臺"))
+    lokiRst = LokiResult([x.replace("台", "臺") for x in inputLIST], filterLIST)
     if lokiRst.getStatus():
-        resultDICT = {"answer": ""}
-        for i in range(0, lokiRst.getLen()):
-            # Weather
-            if lokiRst.getIntent(i) == "Weather":
-                resultDICT = Loki_Weather.getResult(lokiRst.getPattern(i), lokiRst.getUtterance(i), lokiRst.getArgs(i), resultDICT)
+        for index, key in enumerate(inputLIST):
+            for resultIndex in range(0, lokiRst.getLokiLen(index)):
+                # Weather
+                if lokiRst.getIntent(index, resultIndex) == "Weather":
+                    resultDICT = Loki_Weather.getResult(key, lokiRst.getUtterance(index, resultIndex), lokiRst.getArgs(index, resultIndex), resultDICT)
+
     else:
         resultDICT = {"msg": lokiRst.getMessage()}
-
     return resultDICT
 
+def testLoki(inputLIST, filterLIST):
+    INPUT_LIMIT = 20
+    for i in range(0, math.ceil(len(inputLIST) / INPUT_LIMIT)):
+        resultDICT = runLoki(inputLIST[i*INPUT_LIMIT:(i+1)*INPUT_LIMIT], filterLIST)
+
+
 if __name__ == "__main__":
-    #input_str = "後天早上台北適合慢跑嗎"
-    input_str = "明天台北可以不用帶傘嗎"
-    print("Input:", input_str)
-    resultDICT = runLoki(input_str)
-    if "answer" in resultDICT:
-        print("Output:", resultDICT["answer"])
-    else:
-        print("Error:", resultDICT["msg"])
+    # Weather
+    print("[TEST] Weather")
+    inputLIST = [
+                 '今天台北熱不熱','今天台北會下雨嗎',
+                 '今天台北需不需要帶傘',
+                 '今天台北可以不用帶傘嗎',
+                 #'今天台北需不需要帶陽傘',
+                 '台北今天可以不用帶傘嗎',
+                 '後天晚上台北適合慢跑嗎',
+                 '今天台北中午過後天氣如何'
+                ]
+    #testLoki(inputLIST, ['Weather'])
+    #print("")
+
+    # 輸入其它句子試看看
+    #inputLIST = ["輸入你的內容1", "輸入你的內容2"]
+    filterLIST = []
+    resultDICT = runLoki(inputLIST, filterLIST)
+    print("Result => ")
+    pprint(resultDICT)
--- WeatherMan/intent/Loki_Weather.py
+++ WeatherMan/intent/Loki_Weather.py
@@ -5,7 +5,8 @@
     Loki module for Weather
 
     Input:
-        pattern       str,
+        inputSTR      str,
+        utterance     str,
         args          str[],
         resultDICT    dict
 
@@ -13,29 +14,32 @@
         resultDICT    dict
 """
 
-import datetime
-import glob
-import os
 import json
+import os
 import requests
 
+from datetime import datetime
+from glob import glob
 
-BASEPATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CityDICT = glob.glob("{}/datastore/*.json".format(BASEPATH))
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
 
-datetimeFMT = "%Y-%m-%d %H:%M:%S"
-defualtDatetime = datetime.datetime.now()
+DEBUG_Weather = True
+userDefinedDICT = {}
 
 try:
-    infoPath = "{}/account.info".format(os.path.dirname(os.path.abspath(__file__))).replace("/WeatherMan/intent", "")
+    infoPath = "{}/account.info".format(BASEPATH.replace("/WeatherMan/intent", ""))
     infoDICT = json.load(open(infoPath, "r"))
     USERNAME = infoDICT["username"]
     API_KEY = infoDICT["api_key"]
 except:
-    # HINT: 在這裡填入您在 https://api.droidtown.co 的帳號、Articut 的 API_Key 以及 Loki 專案的 Loki_Key
+    # HINT: 在這裡填入您在 https://api.droidtown.co 的帳號、Articut 的 API_Key
     USERNAME = ""
     API_KEY = ""
 
+CityDICT = glob("{}/datastore/*.json".format(os.path.dirname(BASEPATH)))
+
+datetimeFMT = "%Y-%m-%d %H:%M:%S"
+defualtDatetime = datetime.now()
 
 def getCityForecastDict(city):
     forecastDICT = {}
@@ -62,7 +66,7 @@ def getDatetime(inputSTR, timeRef=None):
 
 def convertDatetime2ForecastFMT(datetimeSTR):
     try:
-        resultDatetime = datetime.datetime.strptime(getDatetime(datetimeSTR)[0]["datetime"], datetimeFMT)
+        resultDatetime = datetime.strptime(getDatetime(datetimeSTR)[0]["datetime"], datetimeFMT)
         if datetimeSTR in ["今天", "今日"]:
             resultDatetime = resultDatetime.replace(hour=12)
         return resultDatetime.strftime(datetimeFMT)
@@ -70,16 +74,14 @@ def convertDatetime2ForecastFMT(datetimeSTR):
         return ""
 
 
-DEBUG_Weather = True
 # 將符合句型的參數列表印出。這是 debug 或是開發用的。
-def debugInfo(pattern, utterance, args):
+def debugInfo(inputSTR, utterance):
     if DEBUG_Weather:
-        print("[Weather]\n{} ===> {}\n".format(utterance, args))
-        #print("Pattern: ", pattern)
+        print("[Weather] {} ===> {}".format(inputSTR, utterance))
 
-def getResult(pattern, utterance, args, resultDICT):
-    debugInfo(pattern, utterance, args)
-
+def getResult(inputSTR, utterance, args, resultDICT):
+    debugInfo(inputSTR, utterance)
+    resultDICT[inputSTR] = {"answer": ""}
     if utterance == "[今天][台北]熱不[熱]":
         forecastDICT = getCityForecastDict(args[1])
         queryDatetime = convertDatetime2ForecastFMT(args[0])
@@ -87,7 +89,7 @@ def getResult(pattern, utterance, args, resultDICT):
             #if weatherElement["elementName"] == "WeatherDescription":
                 #for elementTime in weatherElement["time"]:
                     #if queryDatetime >= elementTime["startTime"] and queryDatetime <= elementTime["endTime"]:
-                        #resultDICT["WeatherDescription"] = elementTime["elementValue"][0]["value"]
+                        #resultDICT[inputSTR]["WeatherDescription"] = elementTime["elementValue"][0]["value"]
                         #break
             if weatherElement["elementName"] == "Td":
                 for elementTime in weatherElement["time"]:
@@ -99,33 +101,33 @@ def getResult(pattern, utterance, args, resultDICT):
                         try:
                             value = int(elementTime["elementValue"][0]["value"])
                             if value >= 31:
-                                resultDICT["answer"] += "平均露點溫度為攝氏 {} 度，氣溫非常悶熱，容易中暑，請盡量補充水份。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "平均露點溫度為攝氏 {} 度，氣溫非常悶熱，容易中暑，請盡量補充水份。\n".format(value)
                                 if "熱" in args[2]:
-                                    resultDICT["answer"] = "非常熱。\n" + resultDICT["answer"]
+                                    resultDICT[inputSTR]["answer"] = "非常熱。\n" + resultDICT[inputSTR]["answer"]
                                 elif "冷" in args[2]:
-                                    resultDICT["answer"] = "不會冷，但非常熱。\n" + resultDICT["answer"]
+                                    resultDICT[inputSTR]["answer"] = "不會冷，但非常熱。\n" + resultDICT[inputSTR]["answer"]
                             elif value >= 27:
-                                resultDICT["answer"] += "平均露點溫度為攝氏 {} 度，氣溫較為悶熱。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "平均露點溫度為攝氏 {} 度，氣溫較為悶熱。\n".format(value)
                             elif value >= 20:
-                                resultDICT["answer"] += "平均露點溫度為攝氏 {} 度，氣溫較為舒適。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "平均露點溫度為攝氏 {} 度，氣溫較為舒適。\n".format(value)
                             else:
-                                resultDICT["answer"] += "平均露點溫度為攝氏 {} 度，氣溫稍有寒意。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "平均露點溫度為攝氏 {} 度，氣溫稍有寒意。\n".format(value)
                                 if "熱" in args[2]:
-                                    resultDICT["answer"] = "不會熱。\n" + resultDICT["answer"]
+                                    resultDICT[inputSTR]["answer"] = "不會熱。\n" + resultDICT[inputSTR]["answer"]
                                 elif "冷" in args[2]:
-                                    resultDICT["answer"] = "會冷，若要外出建議穿著外套。\n" + resultDICT["answer"]
+                                    resultDICT[inputSTR]["answer"] = "會冷，若要外出建議穿著外套。\n" + resultDICT[inputSTR]["answer"]
                         except:
                             pass
                         break
 
-    if utterance == "[今天][台北]會下雨嗎":
+    if utterance == "[今天][台北][會]下雨嗎":
         forecastDICT = getCityForecastDict(args[1])
         queryDatetime = convertDatetime2ForecastFMT(args[0])
         for weatherElement in forecastDICT["weatherElement"]:
             #if weatherElement["elementName"] == "WeatherDescription":
                 #for elementTime in weatherElement["time"]:
                     #if queryDatetime >= elementTime["startTime"] and queryDatetime <= elementTime["endTime"]:
-                        #resultDICT["WeatherDescription"] = elementTime["elementValue"][0]["value"]
+                        #resultDICT[inputSTR]["WeatherDescription"] = elementTime["elementValue"][0]["value"]
                         #break
             if weatherElement["elementName"] == "PoP12h":
                 for elementTime in weatherElement["time"]:
@@ -133,21 +135,21 @@ def getResult(pattern, utterance, args, resultDICT):
                         try:
                             value = int(elementTime["elementValue"][0]["value"])
                             if value >= 70:
-                                resultDICT["answer"] += "有 {}% 機率降雨，不建議進行戶外活動。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，不建議進行戶外活動。\n".format(value)
                             else:
-                                resultDICT["answer"] += "有 {}% 機率降雨，可以外出活動。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，可以外出活動。\n".format(value)
                         except:
                             pass
                         break
 
-    if utterance == "[台北][今天]可以不用帶傘嗎":
+    if utterance == "[台北][今天][可以]不用帶傘嗎":
         forecastDICT = getCityForecastDict(args[0])
         queryDatetime = convertDatetime2ForecastFMT(args[1])
         for weatherElement in forecastDICT["weatherElement"]:
             #if weatherElement["elementName"] == "WeatherDescription":
                 #for elementTime in weatherElement["time"]:
                     #if queryDatetime >= elementTime["startTime"] and queryDatetime <= elementTime["endTime"]:
-                        #resultDICT["WeatherDescription"] = elementTime["elementValue"][0]["value"]
+                        #resultDICT[inputSTR]["WeatherDescription"] = elementTime["elementValue"][0]["value"]
                         #break
             if weatherElement["elementName"] == "PoP12h":
                 for elementTime in weatherElement["time"]:
@@ -155,21 +157,21 @@ def getResult(pattern, utterance, args, resultDICT):
                         try:
                             value = int(elementTime["elementValue"][0]["value"])
                             if value >= 50:
-                                resultDICT["answer"] += "有 {}% 機率降雨，建議攜帶雨具。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，建議攜帶雨具。\n".format(value)
                             else:
-                                resultDICT["answer"] += "有 {}% 機率降雨，若短時間外出可以不帶傘。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，若短時間外出可以不帶傘。\n".format(value)
                         except:
                             pass
                         break
 
-    if utterance == "[今天][台北]可以不用帶傘嗎":
+    if utterance == "[今天][台北][可以]不用帶傘嗎":
         forecastDICT = getCityForecastDict(args[1])
         queryDatetime = convertDatetime2ForecastFMT(args[0])
         for weatherElement in forecastDICT["weatherElement"]:
             #if weatherElement["elementName"] == "WeatherDescription":
                 #for elementTime in weatherElement["time"]:
                     #if queryDatetime >= elementTime["startTime"] and queryDatetime <= elementTime["endTime"]:
-                        #resultDICT["WeatherDescription"] = elementTime["elementValue"][0]["value"]
+                        #resultDICT[inputSTR]["WeatherDescription"] = elementTime["elementValue"][0]["value"]
                         #break
             if weatherElement["elementName"] == "PoP12h":
                 for elementTime in weatherElement["time"]:
@@ -177,9 +179,9 @@ def getResult(pattern, utterance, args, resultDICT):
                         try:
                             value = int(elementTime["elementValue"][0]["value"])
                             if value >= 50:
-                                resultDICT["answer"] += "有 {}% 機率降雨，建議攜帶雨具。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，建議攜帶雨具。\n".format(value)
                             else:
-                                resultDICT["answer"] += "有 {}% 機率降雨，若短時間外出可以不帶傘。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，若短時間外出可以不帶傘。\n".format(value)
                         except:
                             pass
                         break
@@ -191,7 +193,7 @@ def getResult(pattern, utterance, args, resultDICT):
             #if weatherElement["elementName"] == "WeatherDescription":
                 #for elementTime in weatherElement["time"]:
                     #if queryDatetime >= elementTime["startTime"] and queryDatetime <= elementTime["endTime"]:
-                        #resultDICT["WeatherDescription"] = elementTime["elementValue"][0]["value"]
+                        #resultDICT[inputSTR]["WeatherDescription"] = elementTime["elementValue"][0]["value"]
                         #break
             if weatherElement["elementName"] == "PoP12h":
                 for elementTime in weatherElement["time"]:
@@ -199,9 +201,9 @@ def getResult(pattern, utterance, args, resultDICT):
                         try:
                             value = int(elementTime["elementValue"][0]["value"])
                             if value >= 50:
-                                resultDICT["answer"] += "有 {}% 機率降雨，建議攜帶雨具。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，建議攜帶雨具。\n".format(value)
                             else:
-                                resultDICT["answer"] += "有 {}% 機率降雨，若短時間外出可以不帶傘。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，若短時間外出可以不帶傘。\n".format(value)
                         except:
                             pass
                         break
@@ -217,18 +219,18 @@ def getResult(pattern, utterance, args, resultDICT):
             if weatherElement["elementName"] == "WeatherDescription":
                 for elementTime in weatherElement["time"]:
                     if queryDatetime >= elementTime["startTime"] and queryDatetime <= elementTime["endTime"]:
-                        resultDICT["WeatherDescription"] = elementTime["elementValue"][0]["value"]
-                        resultDICT["answer"] = "WeatherDescription"
+                        resultDICT[inputSTR]["WeatherDescription"] = elementTime["elementValue"][0]["value"]
+                        resultDICT[inputSTR]["answer"] = "WeatherDescription"
                         break
 
-    if utterance == "[後天][晚上][台北]適合慢跑嗎":
-        forecastDICT = getCityForecastDict(args[2])
-        queryDatetime = convertDatetime2ForecastFMT(args[0]+args[1])
+    if utterance == "[後天晚上][台北][適合]慢跑嗎":
+        forecastDICT = getCityForecastDict(args[1])
+        queryDatetime = convertDatetime2ForecastFMT(args[0])
         for weatherElement in forecastDICT["weatherElement"]:
             #if weatherElement["elementName"] == "WeatherDescription":
                 #for elementTime in weatherElement["time"]:
                     #if queryDatetime >= elementTime["startTime"] and queryDatetime <= elementTime["endTime"]:
-                        #resultDICT["WeatherDescription"] = elementTime["elementValue"][0]["value"]
+                        #resultDICT[inputSTR]["WeatherDescription"] = elementTime["elementValue"][0]["value"]
                         #break
             if weatherElement["elementName"] == "UVI":
                 for elementTime in weatherElement["time"]:
@@ -236,11 +238,11 @@ def getResult(pattern, utterance, args, resultDICT):
                         try:
                             value = int(elementTime["elementValue"][0]["value"])
                             if value >= 7:
-                                resultDICT["answer"] += "紫外線指數 {}，{} 曝曬級數，若要外出請注意防曬及補充水分。\n".format(value, elementTime["elementValue"][1]["value"])
+                                resultDICT[inputSTR]["answer"] += "紫外線指數 {}，{} 曝曬級數，若要外出請注意防曬及補充水分。\n".format(value, elementTime["elementValue"][1]["value"])
                             elif value >= 5:
-                                resultDICT["answer"] += "紫外線指數 {}，{} 曝曬級數，請穿戴衣帽以保護皮膚並在中午陽光強烈時尋找遮蔽處。\n".format(value, elementTime["elementValue"][1]["value"])
+                                resultDICT[inputSTR]["answer"] += "紫外線指數 {}，{} 曝曬級數，請穿戴衣帽以保護皮膚並在中午陽光強烈時尋找遮蔽處。\n".format(value, elementTime["elementValue"][1]["value"])
                             else:
-                                resultDICT["answer"] += "紫外線指數 {}，{} 曝曬級數，屬弱紫外線輻射天氣，無需特別防護。若長期在戶外，建議塗擦SPF在8-12之間的防曬護膚品。\n".format(value, elementTime["elementValue"][1]["value"])
+                                resultDICT[inputSTR]["answer"] += "紫外線指數 {}，{} 曝曬級數，屬弱紫外線輻射天氣，無需特別防護。若長期在戶外，建議塗擦SPF在8-12之間的防曬護膚品。\n".format(value, elementTime["elementValue"][1]["value"])
                         except:
                             pass
                         break
@@ -250,11 +252,12 @@ def getResult(pattern, utterance, args, resultDICT):
                         try:
                             value = int(elementTime["elementValue"][0]["value"])
                             if value >= 70:
-                                resultDICT["answer"] += "有 {}% 機率降雨，不建議進行戶外活動。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，不建議進行戶外活動。\n".format(value)
                             else:
-                                resultDICT["answer"] += "有 {}% 機率降雨，可以外出活動。\n".format(value)
+                                resultDICT[inputSTR]["answer"] += "有 {}% 機率降雨，可以外出活動。\n".format(value)
                         except:
                             pass
                         break
+
 
     return resultDICT
