4ae0bdc209018b08a84339772f317d30ccf96714
qinyanhao <sylvia.wang4837@gmail.com>
2021-09-23 11:18:52 +0800

    update

--- JstormBot/Jstorm_infoBot.py
+++ JstormBot/Jstorm_infoBot.py
@@ -407,7 +407,7 @@ async def on_message(message):
             mscDICT[client.user.id]["updatetime"] = datetimeNow    
         
         #如果member是list，換Group或是換request時清空member
-        if len(lokiResultDICT.keys()) == 1 and type(mscDICT[client.user.id]["member"])==list:
+        if len(lokiResultDICT.keys()) == 1:
             mscDICT[client.user.id]["member"] = ""
             
         #多輪對話
