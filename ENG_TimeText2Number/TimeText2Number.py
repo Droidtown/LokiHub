f29b6a5843055058d1f2541d27e982224f6572cc
Peter <peter.w@droidtown.co>
2021-12-26 20:31:11 +0800

    上傳 TimeText2Number

---
+++ ENG_TimeText2Number/TimeText2Number.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki Template For Python3
+
+    [URL] https://nlu.droidtown.co/Loki/BulkAPI/
+
+    Request:
+        {
+            "username": "your_username",
+            "input_list": ["your_input_1", "your_input_2"],
+            "loki_key": "your_loki_key",
+            "filter_list": ["intent_filter_list"] # optional
+        }
+
+    Response:
+        {
+            "status": True,
+            "msg": "Success!",
+            "version": "v223",
+            "result_list": [
+                {
+                    "status": True,
+                    "msg": "Success!",
+                    "results": [
+                        {
+                            "intent": "intentName",
+                            "pattern": "matchPattern",
+                            "utterance": "matchUtterance",
+                            "argument": ["arg1", "arg2", ... "argN"]
+                        },
+                        ...
+                    ]
+                },
+                {
+                    "status": False,
+                    "msg": "No Match Intent!"
+                }
+            ]
+        }
+"""
+
+from requests import post
+from requests import codes
+import math
+try:
+    from intent import Loki_TimeText
+except:
+    from .intent import Loki_TimeText
+
+import json
+infoDICT = json.loads(open("./account.info", encoding="utf-8").read())
+
+LOKI_URL = "https://nlu.droidtown.co/Loki/BulkAPI/"
+USERNAME = infoDICT["username"]
+LOKI_KEY = infoDICT["apikey"]
+# Filter descrption
+# INTENT_FILTER = []        => All intents (Default)
+# INTENT_FILTER = [intentN] => Only use intent of INTENT_FILTER
+INTENT_FILTER = []
+
+class LokiResult():
+    status = False
+    message = ""
+    version = ""
+    lokiResultLIST = []
+
+    def __init__(self, inputLIST, filterLIST):
+        self.status = False
+        self.message = ""
+        self.version = ""
+        self.lokiResultLIST = []
+        # Default: INTENT_FILTER
+        if filterLIST == []:
+            filterLIST = INTENT_FILTER
+
+        try:
+            result = post(LOKI_URL, json={
+                "username": USERNAME,
+                "input_list": inputLIST,
+                "loki_key": LOKI_KEY,
+                "filter_list": filterLIST
+            })
+
+            if result.status_code == codes.ok:
+                result = result.json()
+                self.status = result["status"]
+                self.message = result["msg"]
+                if result["status"]:
+                    self.version = result["version"]
+                    self.lokiResultLIST = result["result_list"]
+            else:
+                self.message = "{} Connection failed.".format(result.status_code)
+        except Exception as e:
+            self.message = str(e)
+
+    def getStatus(self):
+        return self.status
+
+    def getMessage(self):
+        return self.message
+
+    def getVersion(self):
+        return self.version
+
+    def getLokiStatus(self, index):
+        rst = False
+        if index < len(self.lokiResultLIST):
+            rst = self.lokiResultLIST[index]["status"]
+        return rst
+
+    def getLokiMessage(self, index):
+        rst = ""
+        if index < len(self.lokiResultLIST):
+            rst = self.lokiResultLIST[index]["msg"]
+        return rst
+
+    def getLokiLen(self, index):
+        rst = 0
+        if index < len(self.lokiResultLIST):
+            if self.lokiResultLIST[index]["status"]:
+                rst = len(self.lokiResultLIST[index]["results"])
+        return rst
+
+    def getLokiResult(self, index, resultIndex):
+        lokiResultDICT = None
+        if resultIndex < self.getLokiLen(index):
+            lokiResultDICT = self.lokiResultLIST[index]["results"][resultIndex]
+        return lokiResultDICT
+
+    def getIntent(self, index, resultIndex):
+        rst = ""
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
+            rst = lokiResultDICT["intent"]
+        return rst
+
+    def getPattern(self, index, resultIndex):
+        rst = ""
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
+            rst = lokiResultDICT["pattern"]
+        return rst
+
+    def getUtterance(self, index, resultIndex):
+        rst = ""
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
+            rst = lokiResultDICT["utterance"]
+        return rst
+
+    def getArgs(self, index, resultIndex):
+        rst = []
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
+            rst = lokiResultDICT["argument"]
+        return rst
+
+def runLoki(inputLIST, filterLIST=[]):
+    resultDICT = {}
+    lokiRst = LokiResult(inputLIST, filterLIST)
+    if lokiRst.getStatus():
+        for index, key in enumerate(inputLIST):
+            for resultIndex in range(0, lokiRst.getLokiLen(index)):
+                # TimeText
+                if lokiRst.getIntent(index, resultIndex) == "TimeText":
+                    resultDICT = Loki_TimeText.getResult(key, lokiRst.getUtterance(index, resultIndex), lokiRst.getArgs(index, resultIndex), resultDICT)
+
+    else:
+        resultDICT = {"msg": lokiRst.getMessage()}
+    return resultDICT
+
+def testLoki(inputLIST, filterLIST):
+    INPUT_LIMIT = 20
+    for i in range(0, math.ceil(len(inputLIST) / INPUT_LIMIT)):
+        resultDICT = runLoki(inputLIST[i*INPUT_LIMIT:(i+1)*INPUT_LIMIT], filterLIST)
+
+    if "msg" in resultDICT:
+        print(resultDICT["msg"])
+
+def testIntent():
+    # TimeText
+    print("[TEST] TimeText")
+    inputLIST = ['it is one','one-o-two','one-oh-two',"one o'clock",'two past one','quarter to two','a quarter to two','one twenty-seven','quarter past one',"it is one o'clock",'a quarter past one','twenty four to two','two minutes past one','twenty four minutes to two']
+    testLoki(inputLIST, ['TimeText'])
+    print("")
+
+
+if __name__ == "__main__":
+    # Test all intents
+    #testIntent()
+
+    # Test other
+    inputLIST = ['it is one',                    #1:00
+                 'one-o-two',                    #1:02
+                 'one-oh-two',                   #1:02
+                 "one o'clock",                  #1:00
+                 'two past one',                 #1:02
+                 'quarter to two',               #1:45
+                 'a quarter to two',             #1:45
+                 'one twenty-seven',             #1:27
+                 'quarter past one',             #1:15
+                 "it is one o'clock",            #1:00
+                 'a quarter past one',           #1:15
+                 'twenty-four to two',           #1:36
+                 'two minutes past one',         #1:02
+                 'twenty-four minutes to two'    #1:36
+                 ]
+    filterLIST = []
+    for i in inputLIST:
+        resultDICT = runLoki([i], filterLIST)
+        print("Result => {}".format(resultDICT))
---
+++ ENG_TimeText2Number/intent/Loki_TimeText.py
@@ -0,0 +1,356 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for TimeText
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict
+
+    Output:
+        resultDICT    dict
+"""
+import inflect
+t2n = inflect.engine()
+
+DEBUG_TimeText = True
+userDefinedDICT = {"_timeword":["quarter","o'clock"]}
+
+#using inflect module (https://pypi.org/project/inflect/) to generate clockNumLIST
+clockNumDICT = {"zero": 0,
+                "zero-one"  : 1,
+                "zero-two"  : 2,
+                "zero-three": 3,
+                "zero-four" : 4,
+                "zero-five" : 5,
+                "zero-six"  : 6,
+                "zero-seven": 7,
+                "zero-eight": 8,
+                "zero-nine" : 9,
+                "zero one"  : 1,
+                "zero two"  : 2,
+                "zero three": 3,
+                "zero four" : 4,
+                "zero five" : 5,
+                "zero six"  : 6,
+                "zero seven": 7,
+                "zero eight": 8,
+                "zero nine" : 9,
+                "one"       : 1,
+                "two"       : 2,
+                "three"     : 3,
+                "four"      : 4,
+                "five"      : 5,
+                "six"       : 6,
+                "seven"     : 7,
+                "eight"     : 8,
+                "nine"      : 9,
+                "ten"       : 10,
+                "eleven"    : 11,
+                "twelve"    : 12,
+                "thirteen"  : 13,
+                "fourteen"  : 14,
+                "fifteen"   : 15,
+                "sixteen"   : 16,
+                "seventeen" : 17,
+                "eighteen"  : 18,
+                "nineteen"  : 19,
+                "twenty"    : 20,
+                "twenty-one": 21,
+                "twenty-two": 22,
+                "twenty-three": 23,
+                "twenty-four" : 24,
+                "twenty-five" : 25,
+                "twenty-six"  : 26,
+                "twenty-seven": 27,
+                "twenty-eight": 28,
+                "twenty-nine" : 29,
+                "thirty"      : 30,
+                "thirty-one"  : 31,
+                "thirty-two"  : 32,
+                "thirty-three": 33,
+                "thirty-four" : 34,
+                "thirty-five" : 35,
+                "thirty-six"  : 36,
+                "thirty-seven": 37,
+                "thirty-eight": 38,
+                "thirty-nine" : 39,
+                "forty"       : 40,
+                "forty-one"   : 41,
+                "forty-two"   : 42,
+                "forty-three" : 43,
+                "forty-four"  : 44,
+                "forty-five"  : 45,
+                "forty-six"   : 46,
+                "forty-seven" : 47,
+                "forty-eight" : 48,
+                "forty-nine"  : 49,
+                "fifty"       : 50,
+                "fifty-one"   : 51,
+                "fifty-two"   : 52,
+                "fifty-three" : 53,
+                "fifty-four"  : 54,
+                "fifty-five"  : 55,
+                "fifty-six"   : 56,
+                "fifty-seven" : 57,
+                "fifty-eight" : 58,
+                "fifty-nine"  : 59
+            }
+
+# Debug message
+def debugInfo(inputSTR, utterance):
+    if DEBUG_TimeText:
+        print("[TimeText] {} ===> {}".format(inputSTR, utterance))
+
+def getResult(inputSTR, utterance, args, resultDICT):
+    debugInfo(inputSTR, utterance)
+    if utterance == "[it] is [one]":
+        if args[0].lower() == "it":
+            if len(args[1].split(" ")) == 1:
+                hour = None
+                if args[1] in clockNumDICT.keys():
+                    if clockNumDICT[args[1]] <= 24:
+                        hour = clockNumDICT[args[1]]
+                        resultDICT["time"] = "{} is {:02}:00".format(args[0], hour)
+            elif len(args[1].split(" ")) <= 3:
+                hour = None
+                minute = None
+                if args[1].split(" ")[0] in clockNumDICT.keys() and " ".join(args[1].split(" ")[1:]) in clockNumDICT.keys():
+                    if clockNumDICT[args[1].split(" ")[0]] <= 24 and clockNumDICT[" ".join(args[1].split(" ")[1:])] < 60:
+                        hour = clockNumDICT[args[1].split(" ")[0]]
+                        minute = clockNumDICT[" ".join(args[1].split(" ")[1:])]
+                        resultDICT["time"] = "{:02}:{:02}".format(hour, minute)
+            else:
+                pass
+        else:
+            pass
+
+    if utterance == "[it] is [one] o'clock":
+        if args[0].lower() == "it":
+            if len(args[1].split(" ")) == 1:
+                hour = None
+                if args[1] in clockNumDICT.keys():
+                    if clockNumDICT[args[1]] <= 12:
+                        hour = clockNumDICT[args[1]]
+                        resultDICT["time"] = "{:02}:00".format(hour)
+
+    if utterance == "[one twenty-seven]":
+        #if len(args[0].split(" ")) == 1:                                  #Disabled to prevent a single number under 24 is misinterpretated as time expression.
+            #hour = None
+            #if args[0] in clockNumDICT.keys():
+                #if int(clockNumDICT[args[0]]) <= 24:
+                    #hour = clockNumDICT[args[0]]
+                    #resultDICT["time"] = "{}:00".format(args[0], hour)
+        if 2 <= len(args[0].split(" ")) <= 3:
+            hour = None
+            minute = None
+            if args[0].split(" ")[0] in clockNumDICT.keys() and " ".join(args[0].split(" ")[1:]) in clockNumDICT.keys():
+                if clockNumDICT[args[0].split(" ")[0]] <= 24 and clockNumDICT[" ".join(args[0].split(" ")[1:])] < 60:
+                    hour = clockNumDICT[args[0].split(" ")[0]]
+                    minute = clockNumDICT[" ".join(args[0].split(" ")[1:])]
+                    resultDICT["time"] = "{:02}:{:02}".format(hour, minute)
+        else:
+            pass
+
+    if utterance == "[one] o'clock":
+        if len(args[0].split(" ")) == 1:
+            hour = None
+            if args[0] in clockNumDICT.keys():
+                if clockNumDICT[args[0]] <= 12:
+                    hour = clockNumDICT[args[0]]
+                    resultDICT["time"] = "{:02}:00".format(hour)
+
+    if utterance == "[twenty-four] [to] [two]":
+        if args[1] == "to":
+            if len(args[2].split(" ")) == 1:
+                if args[2] in clockNumDICT.keys():
+                    if clockNumDICT[args[2]] == 1:
+                        hour = 12
+                    elif clockNumDICT[args[2]] <= 12:
+                        hour = int(clockNumDICT[args[2]]) - 1
+                else:
+                    hour = None
+            else:
+                hour = None
+
+            if len(args[0].split(" ")) == 1:
+                if clockNumDICT[args[0]] == 0:
+                    minute = "00"
+                elif args[0] in clockNumDICT.keys():
+                    minute = 60 - clockNumDICT[args[0]]
+                else:
+                    minute = None
+            else:
+                minute = None
+
+            if hour != None and minute != None:
+                resultDICT["time"] = "{:02}:{:02}".format(hour, minute)
+
+
+    if utterance == "[twenty-four] minutes [to] [two]":
+        if args[1] == "to":
+            if len(args[2].split(" ")) == 1:
+                if clockNumDICT[args[2]] <= 12:
+                    if args[2] in clockNumDICT.keys():
+                        if clockNumDICT[args[2]] == 1:
+                            hour = 12
+                        elif int(clockNumDICT[args[2]]) <= 12:
+                            hour = clockNumDICT[args[2]] - 1
+                    else:
+                        hour = None
+                else:
+                    hour = None
+            else:
+                hour = None
+
+            if len(args[0].split(" ")) == 1:
+                if clockNumDICT[args[0]] == 0:
+                    minute = "00"
+                elif args[0] in clockNumDICT.keys():
+                    minute = 60 - clockNumDICT[args[0]]
+                else:
+                    minute = None
+            else:
+                minute = None
+
+            if hour != None and minute != None:
+                resultDICT["time"] = "{:02}:{:02}".format(hour, minute)
+
+    if utterance == "[two] [past] [one]":
+        if args[1] == "past":
+            if len(args[2].split(" ")) == 1:
+                if clockNumDICT[args[2]] <= 24:
+                    if args[2] in clockNumDICT.keys():
+                        hour = clockNumDICT[args[2]]
+                    else:
+                        hour = None
+                else:
+                    hour = None
+            else:
+                hour = None
+
+            if len(args[0].split(" ")) == 1:
+                if clockNumDICT[args[0]] == 0:
+                    minute = "00"
+                elif args[0] in clockNumDICT.keys():
+                    minute = clockNumDICT[args[0]]
+                else:
+                    minute = None
+            else:
+                minute = None
+
+            if hour != None and minute != None:
+                resultDICT["time"] = "{:02}:{:02}".format(hour, minute)
+
+    if utterance == "[two] minutes [past] [one]":
+        if args[1] == "past":
+            if len(args[2].split(" ")) == 1:
+                if clockNumDICT[args[2]] <= 24:
+                    if args[2] in clockNumDICT.keys():
+                        hour = clockNumDICT[args[2]]
+                    else:
+                        hour = None
+                else:
+                    hour = None
+            else:
+                hour = None
+
+            if len(args[0].split(" ")) == 1:
+                if clockNumDICT[args[0]] == 0:
+                    minute = "00"
+                elif args[0] in clockNumDICT.keys():
+                    minute = clockNumDICT[args[0]]
+                else:
+                    minute = None
+            else:
+                minute = None
+
+            if hour != None and minute != None:
+                resultDICT["time"] = "{:02}:{:02}".format(hour, minute)
+
+    if utterance == "a quarter [past] [one]":
+        if args[0] == "past":
+            if len(args[1].split(" ")) == 1:
+                if clockNumDICT[args[1]] <= 24:
+                    if args[1] in clockNumDICT.keys():
+                        hour = clockNumDICT[args[1]]
+                    else:
+                        hour = None
+                else:
+                    hour = None
+            else:
+                hour = None
+
+            if hour != None:
+                resultDICT["time"] = "{:02}:15".format(hour)
+
+    if utterance == "a quarter [to] [two]":
+        if args[0] == "to":
+            if len(args[1].split(" ")) == 1:
+                if args[1] in clockNumDICT.keys():
+                    if int(clockNumDICT[args[1]]) == 1:
+                        hour = 12
+                    elif clockNumDICT[args[1]] <= 12:
+                        hour = clockNumDICT[args[1]] - 1
+                else:
+                    hour = None
+            else:
+                hour = None
+
+            if hour != None:
+                resultDICT["time"] = "{:02}:45".format(hour)
+
+    if utterance == "one-o-two":
+        try:
+            if len(utterance.split("-")) == 3:
+                argLIST = utterance.split("-")
+                if argLIST[1] in ("o", "O", "oh"):
+                    if argLIST[0] in clockNumDICT.keys() and argLIST[2] in clockNumDICT.keys():
+                        if clockNumDICT[argLIST[0]] <= 24 and clockNumDICT[argLIST[2]] < 10:
+                            resultDICT["time"] = "{:02}:{:02}".format(clockNumDICT[argLIST[0]], clockNumDICT[argLIST[2]])
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+        except:
+            pass
+
+
+    if utterance == "quarter [past] [one]":
+        if args[0] == "past":
+            if len(args[1].split(" ")) == 1:
+                if clockNumDICT[args[1]] <= 24:
+                    if args[1] in clockNumDICT.keys():
+                        hour = clockNumDICT[args[1]]
+                    else:
+                        hour = None
+                else:
+                    hour = None
+            else:
+                hour = None
+
+            if hour != None:
+                resultDICT["time"] = "{:02}:15".format(hour)
+
+    if utterance == "quarter [to] [two]":
+        if args[0] == "to":
+            if len(args[1].split(" ")) == 1:
+                if args[1] in clockNumDICT.keys():
+                    if clockNumDICT[args[1]] == 1:
+                        hour = 12
+                    elif clockNumDICT[args[1]] <= 12:
+                        hour = clockNumDICT[args[1]] - 1
+                else:
+                    hour = None
+            else:
+                hour = None
+
+            if hour != None:
+                resultDICT["time"] = "{:02}:45".format(hour)
+
+    return resultDICT
