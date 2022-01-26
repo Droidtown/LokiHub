cfbf67ea49a8a745ee65ed30b2f7ae816c992f26
Keith LAI <keith.l@droidtown.co>
2022-01-26 16:17:45 +0800

    移除檔案

--- BeautiBot/Discord/intent/account.info.py
+++
@@ -1,4 +0,0 @@
-{"username": "nienhengwu@gmail.com", 
- "articut_api_key": "X1#CfW!S^1z%ncCakreZ^ys%j42@6BX", 
- "loki_project_key": "25Abbm6oSPP*RrBRrqUp&Jfe+ASBYRI",
- "discord_token": "ODgwNzM3ODQ0MTQ1MDI5MTMw.YSipCQ.xxjy5N7RMeqxdCgadk0Q9-Jypco"}
