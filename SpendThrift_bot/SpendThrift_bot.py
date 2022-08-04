c4e61a7f1446055827b7252cecf45fc72fde4a6d
Grammyship <ss96083@gmail.com>
2022-08-05 01:34:08 +0800

    fix(SpendThrift_bot.py): login

--- SpendThrift_bot/SpendThrift_bot.py
+++ SpendThrift_bot/SpendThrift_bot.py
@@ -62,7 +62,7 @@ accountDICT = json.load(open("account.info", encoding="utf-8"))
 
 LOKI_URL = "https://api.droidtown.co/Loki/BulkAPI/"
 USERNAME = accountDICT["UserName"]
-LOKI_KEY = accountDICT["LokiKey"]
+LOKI_KEY = "BbcY-sJJE-bmc&^s!wZuXCxmzoLeHUh"
 # 意圖過濾器說明
 # INTENT_FILTER = []        => 比對全部的意圖 (預設)
 # INTENT_FILTER = [intentN] => 僅比對 INTENT_FILTER 內的意圖
