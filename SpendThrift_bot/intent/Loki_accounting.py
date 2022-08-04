ac1ea7b351e897ee5ae1828b4d66fde683d77f67
Grammyship <ss96083@gmail.com>
2022-08-05 01:16:01 +0800

    feat(Loki_accounting.py):  參數綁定

--- SpendThrift_bot/intent/Loki_accounting.py
+++ SpendThrift_bot/intent/Loki_accounting.py
@@ -31,59 +31,59 @@ def debugInfo(inputSTR, utterance):
 def getResult(inputSTR, utterance, args, resultDICT):
     debugInfo(inputSTR, utterance)
     if utterance == "[50040][支出]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[0]
+        resultDICT["account"] = args[1]
 
     if utterance == "[50040元][支出]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[0][:-1]
+        resultDICT["account"] = args[1]
 
     if utterance == "[我]跟[我]朋友去814閒逛了[兩個小時]，[支出][3000元]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[4]
+        resultDICT["account"] = args[3][:-1]
 
     if utterance == "[支出][12000]，醫藥費":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1]
+        resultDICT["account"] = args[0]
 
     if utterance == "[支出][12000元]，醫藥費":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1][:-1]
+        resultDICT["account"] = args[0]
 
     if utterance == "[支出][2000元]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1][:-1]
+        resultDICT["account"] = args[0]
 
     if utterance == "[收入][15000]，[中]大樂透":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1]
+        resultDICT["account"] = args[0]
 
     if utterance == "[收入][15000元]，[中]大樂透":
-        # write your code here
-        pass
-
+        resultDICT["amount"] = args[1][:-1]
+        resultDICT["account"] = args[0]
+        
     if utterance == "[收入][3000]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1]
+        resultDICT["account"] = args[0]
 
     if utterance == "去814[支出][300]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1]
+        resultDICT["account"] = args[0]
 
     if utterance == "去814[支出][300元]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1][:-1]
+        resultDICT["account"] = args[0]
 
     if utterance == "去[全聯][支出][3200元]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[2][:-1]
+        resultDICT["account"] = args[1]
 
     if utterance == "去小7[收入][1300元]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1][:-1]
+        resultDICT["account"] = args[0]
 
     if utterance == "去小七[支出][3400]":
-        # write your code here
-        pass
+        resultDICT["amount"] = args[1]
+        resultDICT["account"] = args[0]
 
     return resultDICT
