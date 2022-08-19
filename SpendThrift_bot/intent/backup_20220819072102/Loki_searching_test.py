81302279a247f7280ba71becbe9309fa3029b645
Grammyship <ss96083@gmail.com>
2022-08-19 15:22:23 +0800

    Update for droidtown testing

---
+++ SpendThrift_bot/intent/backup_20220819072102/Loki_searching_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for searching_test
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict
+
+    Output:
+        resultDICT    dict
+"""
+
+import json
+import os
+
+DEBUG_searching_test = True
+try:
+    userDefinedDICT = json.load(open(os.path.join(os.path.dirname(__file__), "USER_DEFINED.json"), encoding="utf-8"))
+except:
+    userDefinedDICT = {"_key":["收入","支出","記帳狀況"],"_park":["六福村","九族文化村","義大","義大世界"],"money":["支出總額","支出費用","總金額","總額","費用","金錢","錢"]}
+
+# 將符合句型的參數列表印出。這是 debug 或是開發用的。
+def debugInfo(inputSTR, utterance):
+    if DEBUG_searching_test:
+        print("[searching_test] {} ===> {}".format(inputSTR, utterance))
+
+def getResult(inputSTR, utterance, args, resultDICT):
+    debugInfo(inputSTR, utterance)
+    if utterance == "查詢[收入]":
+        # write your code here
+        pass
+
+    return resultDICT
