d601176644aaba04727048b377aee2d1c5eb2462
byen2 <benoit.yen@sjsu.edu>
2022-08-23 17:03:34 -0700

    update dividend intent

--- ENG_MathWordProblem_Solver/Loki/intent/Loki_dividend.py
+++ ENG_MathWordProblem_Solver/Loki/intent/Loki_dividend.py
@@ -63,21 +63,6 @@ def value2variable(resultDICT, entitySTR, variableX, numberSTR):
 
 def getResult(inputSTR, utterance, args, resultDICT):
     debugInfo(inputSTR, utterance)
-    if utterance == "The [school] have [$20,000]":
-        # write your code here
-        pass
-
-    if utterance == "[Melissa] buy [2] pack [of] [tennis] [balls] [for] [$12] [in] [total]":
-        # write your code here
-        pass
-
-    if utterance == "[you] have [4] [pieces of] [candy] split [evenly] [into] [2] [bags]":
-        # write your code here
-        pass
-
-    if utterance == "[you] have [80] [tickets] [for] the [fair] and [each] ride costs [5] [tickets]":
-        # write your code here
-        pass
 
     if utterance == "[Allan] have [49] [blocks] and there be [7] [blocks] [for] [every] [color]":
         if inputSTR in resultDICT["inputStrLIST"]:
