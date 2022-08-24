74500605160ba51065b23a1f88843bc874ef41e1
byen2 <benoit.yen@sjsu.edu>
2022-08-23 17:00:14 -0700

    update divisor/dividend intent + refs

--- ENG_MathWordProblem_Solver/Loki/intent/Loki_divisor.py
+++ ENG_MathWordProblem_Solver/Loki/intent/Loki_divisor.py
@@ -29,90 +29,203 @@ def debugInfo(inputSTR, utterance):
     if DEBUG_divisor:
         print("[divisor] {} ===> {}".format(inputSTR, utterance))
 
+def entity2symbol(resultDICT, entitySTR):
+    '''
+    Assign Symbols to resultDICT["symbolDICT"]
+    '''
+    entitySTR = sgForm(entitySTR)
+    if entitySTR in resultDICT["symbolDICT"].keys():
+        pass
+    else:
+        resultDICT["symbolDICT"][entitySTR] = []
+    return resultDICT
+
+def symbol2variable(resultDICT, entitySTR):
+    '''
+    Assign variables to Symbols in resultDICT["symbolDICT"]
+    '''
+    entitySTR = sgForm(entitySTR)
+    if "{}_{}".format(sympyKey(entitySTR), len(resultDICT["symbolDICT"][entitySTR])) in resultDICT["symbolDICT"][entitySTR]:
+        variableX = sympy.Symbol("{}_{}".format(sympyKey(entitySTR), len(resultDICT["symbolDICT"][entitySTR])+1))
+    else:
+        variableX = sympy.Symbol("{}_{}".format(sympyKey(entitySTR), len(resultDICT["symbolDICT"][entitySTR])))
+    return variableX
+
+def value2variable(resultDICT, entitySTR, variableX, numberSTR):
+    '''
+    Assign value to the variable
+    '''
+    entitySTR = sgForm(entitySTR)
+    number = numExtractor(numberSTR)
+    resultDICT["symbolDICT"][entitySTR].append((variableX, number))
+    resultDICT["symbolLIST"].append((variableX, number))
+    return resultDICT
+
 def getResult(inputSTR, utterance, args, resultDICT):
     debugInfo(inputSTR, utterance)
-    if utterance == "[Melissa] buy [2] pack [of] [tennis] [balls]":
-        # write your code here
-        pass
 
     if utterance == "[each] [piece] [of] [equipment] cost [$50]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
 
-    if utterance == "[you] have [4] [pieces of] [candy] split [evenly] [into] [2] [bags]":
-        # write your code here
-        pass
-
-    if utterance == "[you] have [80] [tickets] [for] the [fair] and [each] ride costs [5] [tickets]":
-        # write your code here
-        pass
-
-    if utterance == "there be [6] [tennis] [balls]":
-        # write your code here
-        pass
+        if args[0] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = "money_{}".format(sgForm(args[3]))
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[4])
 
     if utterance == "[Every] [crayon] [box] can contain [8] [crayons]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
+
+        if args[0] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[4])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[3])
 
     if utterance == "[If] [14 pieces of] [stone] be [in] [each] [bracelet]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
 
-    if utterance == "[If] brand A be $50 [for] [5 kg]":
-        # write your code here
-        pass
-
-    if utterance == "[Melissa] buy [2 packs of] [tennis] [balls]":
-        # write your code here
-        pass
+        if args[4] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[2])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[1])
 
     if utterance == "[each] [glass] need [two] [lemons]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
 
-    if utterance == "[each] piece [of] [equipment] cost [$50]":
-        # write your code here
-        pass
+        if args[0] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[3])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[2])
 
-    if utterance == "[equal] number [of] [paintings] [in] [four] [of] the [rooms]":
-        # write your code here
-        pass
+    if utterance == "[equal] [number] [of] [paintings] [in] [four] [of] the [rooms]":
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
+
+        if args[0] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[7])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[5])
 
     if utterance == "[every] [box] can contain [100] [sheets]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
+
+        if args[0] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[3])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[2])
 
     if utterance == "[every] [packet] need [to] contain [7] [seeds]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
+
+        if args[0] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[4])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[3])
 
     if utterance == "[it] take [3] [cutlets] [to] make a [dish]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
+
+        # 1. Assign Symbols
+        entityX = sgForm(args[2])
+        resultDICT = entity2symbol(resultDICT, entityX)
+        # 2. Assign variables to Symbols
+        resultDICT = value2variable(resultDICT, entityX, x, entityX)
+        # 3. Assign value to Symbols
+        resultDICT = value2variable(resultDICT, entityX, x, args[1])
 
     if utterance == "[paper] [envelopes] which can hold [10] [papers] [each]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
+
+        if args[4] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[3])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[2])
 
     if utterance == "[she] give [to] [each] [of] her [two] [grandmothers]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
+
+        if args[2] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[5])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[4])
 
     if utterance == "[they] leave [exactly] [7] [nails] [in] [every] [station]":
-        # write your code here
-        pass
+        if inputSTR in resultDICT["inputStrLIST"]:
+            return resultDICT
+        else:
+            resultDICT["inputStrLIST"].append(inputSTR)
 
-    if utterance == "[you] have [4 pieces of] [candy] split [evenly] [into] [2] [bags]":
-        # write your code here
-        pass
-
-    if utterance == "[you] have [80] [tickets] [for] the [fair] and [each] [ride] costs [5] [tickets]":
-        # write your code here
-        pass
-
-    if utterance == "brand B be $48 [per] [4 kg]":
-        # write your code here
-        pass
+        if args[5] in ("every", "each", "one", "evenly", "per", "for"):
+            # 1. Assign Symbols
+            entityX = sgForm(args[3])
+            resultDICT = entity2symbol(resultDICT, entityX)
+            # 2. Assign variables to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, entityX)
+            # 3. Assign value to Symbols
+            resultDICT = value2variable(resultDICT, entityX, x, args[2])
 
     return resultDICT
