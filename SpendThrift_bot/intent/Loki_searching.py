6dc49a63aa61b1c9eddec6ab775962b612c48d4c
Grammyship <ss96083@gmail.com>
2022-10-02 20:56:20 +0800

    add(Loki_searching.py): 新增搜尋句型

--- SpendThrift_bot/intent/Loki_searching.py
+++ SpendThrift_bot/intent/Loki_searching.py
@@ -25,7 +25,7 @@ DEBUG_searching = True
 try:
     userDefinedDICT = json.load(open(os.path.join(os.path.dirname(__file__), "USER_DEFINED.json"), encoding="utf-8"))
 except:
-    userDefinedDICT = {"_key":["收入","支出","記帳狀況"],"_park":["六福村","九族文化村","義大","義大世界"],"_money":["支出總額","支出費用","總金額","總額","費用","金錢","錢"]}
+    userDefinedDICT = {"_key":["收入","支出","記帳狀況"],"_park":["六福村","九族文化村","義大","義大世界"],"_money":["錢","台幣","元"],"_income_key":["收入"],"_allcome_key":["記帳","財富"],"_outcome_key":["支出","消費","花費"]}
 
 
 
@@ -40,47 +40,129 @@ def getResult(userID, inputSTR, utterance, args, resultDICT):
     debugInfo(inputSTR, utterance)
 
     if utterance == "查詢[收入]":
-        if args[0] in ["支出"]:
+        if args[0] in userDefinedDICT["_outcome_key"]:
             result = fun.GetDataByCondition(userID, "cost")
-        elif args[0] in ["收入"]:
+        elif args[0] in userDefinedDICT["_income_key"]:
             result = fun.GetDataByCondition(userID, "earn")
-        elif args[0] in ["記帳狀況"]:
+        elif args[0] in userDefinedDICT["_allcome_key"]:
             result = fun.GetDataByCondition(userID, "all")
-        
-        pass
+
+    if utterance == "[我][今年][總共]花了多少[錢]":
+        if not fun.CorrectCurrency(args[3],userDefinedDICT["_money"]):
+            result = "error_currency"
+        else:
+            result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan(args[1]))
+
+    if utterance == "[我][今年][總共]賺了多少[錢]":
+        if not fun.CorrectCurrency(args[3],userDefinedDICT["_money"]):
+            result = "error_currency"
+        result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan(args[1]))
+
+    if utterance == "[我][總共]花了多少[錢]":
+        if not fun.CorrectCurrency(args[2],userDefinedDICT["_money"]):
+            result = "error_currency"
+        result = fun.GetDataByCondition(userID, "cost")
+
+    if utterance == "[我][總共]賺了多少[錢]":
+        if not fun.CorrectCurrency(args[2],userDefinedDICT["_money"]):
+            result = "error_currency"
+        result = fun.GetDataByCondition(userID, "earn")
+
+    if utterance == "[我][這個][月][總共]花了多少[錢]":
+        if not fun.CorrectCurrency(args[4],userDefinedDICT["_money"]):
+            result = "error_currency"
+        result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan("_articut_failed_",[args[1],args[2]]))
+
+    if utterance == "[我][這個][月][總共]賺了多少[錢]":
+        if not fun.CorrectCurrency(args[4],userDefinedDICT["_money"]):
+            result = "error_currency"
+        result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan("_articut_failed_", [args[1],args[2]]))
+
+    if utterance == "查詢[今日][收入]":
+        if args[1] in userDefinedDICT["_outcome_key"]:
+            result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan(args[0]))
+        elif args[1] in userDefinedDICT["_income_key"]:
+            result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan(args[0]))
+        elif args[1] in userDefinedDICT["_allcome_key"]:
+            result = fun.GetDataByCondition(userID, "all", fun.GetTimeSpan(args[0]))
+
+    if utterance == "查詢[今日][總][收入]":
+        if args[2] in userDefinedDICT["_outcome_key"]:
+            result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan(args[0]))
+        elif args[2] in userDefinedDICT["_income_key"]:
+            result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan(args[0]))
+        elif args[2] in userDefinedDICT["_allcome_key"]:
+            result = fun.GetDataByCondition(userID, "all", fun.GetTimeSpan(args[0]))
+
+    if utterance == "查詢[總][收入]":
+        if args[1] in userDefinedDICT["_outcome_key"]:
+            result = fun.GetDataByCondition(userID, "cost")
+        elif args[1] in userDefinedDICT["_income_key"]:
+            result = fun.GetDataByCondition(userID, "earn")
+        elif args[1] in userDefinedDICT["_allcome_key"]:
+            result = fun.GetDataByCondition(userID, "all")
+            
+    if utterance == "查詢[這個][月][收入]":
+        if args[2] in userDefinedDICT["_outcome_key"]:
+            result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan(args[0]+args[1]))
+        elif args[2] in userDefinedDICT["_income_key"]:
+            result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan(args[0]+args[1]))
+        elif args[2] in userDefinedDICT["_allcome_key"]:
+            result = fun.GetDataByCondition(userID, "all", fun.GetTimeSpan(args[0]+args[1]))
+
+    if utterance == "查詢[這個][月][總][收入]":
+        if args[3] in userDefinedDICT["_outcome_key"]:
+            result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan(args[0]+args[1]))
+        elif args[3] in userDefinedDICT["_income_key"]:
+            result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan(args[0]+args[1]))
+        elif args[3] in userDefinedDICT["_allcome_key"]:
+            result = fun.GetDataByCondition(userID, "all", fun.GetTimeSpan(args[0]+args[1]))
+
+    if utterance == "[我][上個月][總共]花了多少[錢]":
+        if not fun.CorrectCurrency(args[3],userDefinedDICT["_money"]):
+            result = "error_currency"
+        result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan(args[1]))
+
+    if utterance == "[我][上個月][總共]賺了多少[錢]":
+        if not fun.CorrectCurrency(args[3],userDefinedDICT["_money"]):
+            result = "error_currency"
+        result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan(args[1]))
+
+    if utterance == "查詢[我][上週五]的[收入]":
+        if args[2] in userDefinedDICT["_outcome_key"]:
+            result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan(args[1]))
+        elif args[2] in userDefinedDICT["_income_key"]:
+            result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan(args[1]))
+        elif args[2] in userDefinedDICT["_allcome_key"]:
+            result = fun.GetDataByCondition(userID, "all", fun.GetTimeSpan(args[1]))
+
+    if utterance == "查詢[我][這個][月]的[收入]":
+        if args[3] in userDefinedDICT["_outcome_key"]:
+            result = fun.GetDataByCondition(userID, "cost", fun.GetTimeSpan(args[1]+args[2]))
+        elif args[3] in userDefinedDICT["_income_key"]:
+            result = fun.GetDataByCondition(userID, "earn", fun.GetTimeSpan(args[1]+args[2]))
+        elif args[3] in userDefinedDICT["_allcome_key"]:
+            result = fun.GetDataByCondition(userID, "all", fun.GetTimeSpan(args[1]+args[2]))
+
+    if utterance == "查詢[我]的[收入]":
+        if args[1] in userDefinedDICT["_outcome_key"]:
+            result = fun.GetDataByCondition(userID, "cost")
+        elif args[1] in userDefinedDICT["_income_key"]:
+            result = fun.GetDataByCondition(userID, "earn")
+        elif args[1] in userDefinedDICT["_allcome_key"]:
+            result = fun.GetDataByCondition(userID, "all")
     
-
-
-    if utterance == "[我][今年][總共]花了多少錢":
-        result = fun.GetDataByCondition(userID, "cost")
-        pass
-
-    if utterance == "[我][今年][總共]賺了多少錢":
-        result = fun.GetDataByCondition(userID, "earn")
-        pass
-
-    if utterance == "[我][總共]花了多少錢":
-        result = fun.GetDataByCondition(userID, "cost")
-        pass
-
-    if utterance == "[我][總共]賺了多少錢":
-        result = fun.GetDataByCondition(userID, "earn")
-        pass
-
-    if utterance == "[我][這個][月][總共]花了多少錢":
-        result = fun.GetDataByCondition(userID, "cost")
-        pass
-
-    if utterance == "[我][這個][月][總共]賺了多少錢":
-        result = fun.GetDataByCondition(userID, "earn")
-        pass
-
-
+    
     """
     處理結果
     """
     # 錯誤
-    if result == "error":
+    # 幣值錯誤
+    if result == "error_currency":
+        resultDICT["intent"] = result
+        resultDICT["err_msg"] = "錯誤"
+    # 其他錯誤
+    elif result == "error":
         resultDICT["intent"] = "error"
         resultDICT["err_msg"] = "錯誤"
     # 結果
